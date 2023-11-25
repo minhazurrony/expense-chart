3e83a6267fbca7a0fa450be77c3f97d33ca62e77
minhazurrony <minhazur.rony@gmail.com>
2023-11-25 17:00:57 +0600

    feat: added donut chart - added chart - fixed some responsive issues

--- src/App.jsx
+++ src/App.jsx
@@ -1,6 +1,26 @@
-import { useState } from "react";
+import { useState, useEffect } from "react";
+import data from "./data/expense-data.json";
+import { colorMapper } from "./utils/colorMapper";
+import { DonutChart } from "./components/DonutChart";
+
 function App() {
-  const [activeFilter, setActiveFilter] = useState("ALL TIME");
+  const [activeFilter, setActiveFilter] = useState(data[3].period);
+  const [modifiedData, setModifiedData] = useState([]);
+
+  useEffect(() => {
+    const foundData = data.find((item) => item.period === activeFilter);
+
+    const formattedData = Object.keys(foundData)
+      .filter((key) => key !== "period")
+      .map((key) => {
+        return {
+          category: key,
+          value: foundData[key],
+          color: colorMapper(key),
+        };
+      });
+    setModifiedData(formattedData);
+  }, [activeFilter]);
 
   const handleFilter = (event) => {
     setActiveFilter(event.target.value);
@@ -11,17 +31,17 @@ function App() {
         expense chart
       </h1>
 
-      <div className="p-8 bg-neutral_white h-[561px] w-[545px] my-0 mx-[auto] rounded-[40px]">
+      <div className="p-8 bg-neutral_white  w-[calc(100vw-32px)] my-0 mx-[auto] rounded-[40px] md:w-[80vw] lg:w-[60vw] xl:w-[40vw] 2xl:w-[35vw]">
         <h2 className="capitalize text-neutral_black text-[24px] text-center mb-[42px] font-bold">
           expenses
         </h2>
 
-        <div className="w-[291px] h-[56px] mx-[auto] px-8 py-3 bg-light_grey_2 flex flex-row justify-between align-middle rounded-[32px]">
+        <div className="mx-[auto] px-8 py-3 bg-light_grey_2 flex flex-row flex-wrap justify-between items-center rounded-[32px] md:w-[65%] lg:w-[60%]">
           <button
             value={"1M"}
             type="button"
             className={`tracking-[0.84px] uppercase py-1 px-2 text-[12px] font-medium text-dark_grey focus:outline-none ${
-              activeFilter === "1M" && "bg-neutral_white"
+              activeFilter === "1M" && "bg-neutral_white text-blue"
             }  rounded-full hover:bg-neutral_white hover:text-blue`}
             onClick={handleFilter}
           >
@@ -31,7 +51,7 @@ function App() {
             value={"6M"}
             type="button"
             className={`tracking-[0.84px] uppercase py-1 px-2 text-[12px] font-medium text-dark_grey focus:outline-none ${
-              activeFilter === "6M" && "bg-neutral_white"
+              activeFilter === "6M" && "bg-neutral_white text-blue"
             }  rounded-full hover:bg-neutral_white hover:text-blue`}
             onClick={handleFilter}
           >
@@ -41,7 +61,7 @@ function App() {
             value={"1Y"}
             type="button"
             className={`tracking-[0.84px] uppercase py-1 px-2 text-[12px] font-medium text-dark_grey focus:outline-none ${
-              activeFilter === "1Y" && "bg-neutral_white"
+              activeFilter === "1Y" && "bg-neutral_white text-blue"
             }  rounded-full hover:bg-neutral_white hover:text-blue`}
             onClick={handleFilter}
           >
@@ -51,12 +71,38 @@ function App() {
             value={"ALL TIME"}
             type="button"
             className={`tracking-[0.84px] uppercase py-1 px-2 text-[12px] font-medium text-dark_grey focus:outline-none ${
-              activeFilter === "ALL TIME" && "bg-neutral_white"
+              activeFilter === "ALL TIME" && "bg-neutral_white text-blue"
             }  rounded-full hover:bg-neutral_white hover:text-blue`}
             onClick={handleFilter}
           >
             all time
           </button>
+        </div>
+
+        <DonutChart
+          width={300}
+          height={300}
+          items={modifiedData}
+          innerRadius={100}
+          outerRadius={125}
+        />
+        <div className="mt-[47px] flex flex-row flex-wrap">
+          {modifiedData.map((item, idx) => {
+            return (
+              <div
+                className="flex flex-row items-center mr-[22px]"
+                key={`${item.category}-${idx}`}
+              >
+                <div
+                  className={`w-[37px] h-[13px] rounded-[33px] mr-[13px]`}
+                  style={{ backgroundColor: `${item.color}` }}
+                ></div>
+                <p className="text-[16px] text-light_grey_1 font-medium tracking-[-0.32px] capitalize">
+                  {item?.category}
+                </p>
+              </div>
+            );
+          })}
         </div>
       </div>
     </div>
